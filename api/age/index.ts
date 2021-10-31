93bed0cdf9c1d29fd7d06845ff74a564c3cb05a4
Gregor <git@grgr.dev>
2021-10-31 14:12:02 +0100

    cleanup unused file

--- api/age/index.ts
+++
@@ -1,5 +0,0 @@
-import type { APIContext, APIHandler } from "aleph/types.d.ts";
-
-export const handler: APIHandler = (context: APIContext) => {
-  throw new Error("GET on /api/birthdate not implemented");
-};
